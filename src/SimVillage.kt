7af8bffac166a5a629851b29ac15101e79e2cc5b
Yuuki Takahashi <20282867+yktakaha4@users.noreply.github.com>
2019-04-28 23:19:55 +0900

    change to callback function

--- src/SimVillage.kt
+++ src/SimVillage.kt
@@ -1,8 +1,13 @@
 fun main(args: Array<String>) {
-    val greetingFunction: (String, Int) -> String = { playerName, numBuildings ->
+    val greetingFunction = { playerName: String, numBuildings: Int ->
         val currentYear = 2018
         println("Adding $numBuildings houses")
         "Welcome to SimVillage, $playerName! (copyright $currentYear)"
     }
-    println(greetingFunction("Guyal", 2))
+    runSimulation("Guyal", greetingFunction)
+}
+
+fun runSimulation(playerName: String, greetingFunction: (String, Int) -> String) {
+    val numBuildings = (1..3).shuffled().last()
+    println(greetingFunction(playerName, numBuildings))
 }
